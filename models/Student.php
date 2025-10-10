a6b7ee28381bd23cc735abfe48d2a9f97f2ff6a3
IT(K) <phanlebakhang72@gmail.com>
2025-10-10 10:47:24 +0700

    update

--- models/Student.php
+++ models/Student.php
@@ -1,46 +1,40 @@
 <?php
 require_once __DIR__ . '/../config/db.php';
 
-class student{
-    private $pdo;
+class Student{
+    private $conn;
+    private $table ="student";
 
-    public function __construct(){
-
-        $db = new Database();
-        $this->pdo = $db->connect();
+    public function __construct(){ // mo file va data
+        $db = new Data();
+        $this->conn = $db->connect();
     }
 
     public function getAll(){
-        $stmt =$this->pdo->query("SELECT * FROM students ORDER BY id DESC");
-        return $stmt->fetchAll();
-    }
-    public function getById($id){
-        $stmt =$this ->pdo->prepare("SELECT * FROM stundents WHERE id = ?");
-        $stmt->execute([$id]);
-        return $stmt->fetch();
+        $stmt = $this->conn->prepare("SELECT * FROM $this -> table ORDER BY id DESC");
+        $stmt->execute();
+        return $stmt;
     }
 
-    public function create($name, $email,$phone){
-        $stmt = $this->pdo->prepare("INSERT INTO students (name, email, phone) VALUES (?,?,?)");
-        return $stmt->execute([$name,$email,$phone]);
+    public function getById($id){
+$stmt = $this->conn->prepare(" SELECT * FROM $this-> table WHERE id=?");
+$stmt->execute([$id]);
+return $stmt->fetch(PDO::FETCH_ASSOC);
     }
-    public function update($id, $name, $email, $phone) {
-        $stmt = $this->pdo->prepare(" UPDATE students SET name = ?, email = ?, phone = ? WHERE id = ?");
-        return $stmt->execute([$name,$email,$phone,$id]);
+
+    public function add($name,$email,$dob){
+        $stmt = $this->conn->prepare("INSERT INTO $this ->table (name, email,dob) VALUES (?,?,?)");
+        return $stmt->execute([$name,$email,$dob]);
     }
+
+    public function update($id,$name, $email,$dob){
+        $stmt = $this->conn->prepare("UPDATE $this->table SET name=?, email?, dob=?");
+        return $stmt->execute([$name,$email,$dob,$id]);
+    }
+
     public function delete($id){
-        $stmt=$this->pdo->prepare("DELETE FROM student WHERE id = ?");
+        $stmt = $this->conn->prepare("DELETE FROM $this->table WHERE id=?");
         return $stmt->execute([$id]);
     }
-    public function countStudents() {
-        $stmt = $this->pdo->query("SELECT COUNT(*) FROM students");
-        return $stmt->fetchColumn();
-    }
-    public function getCourses($student_id){
-        $stmt=$this->pdo->prepare("SELECT c.* FROM courses c JOIN enrollments e ON c.id = e.course_id WHERE e.student_id = ?");
-    $stmt->executefetchAll();
-    return $stmt->fetchAll();
 }
-}
-
-?>
+?> 
