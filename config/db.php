e0bb7b3592c63f4abc7e308cd450811bfcad843c
IT(K) <phanlebakhang72@gmail.com>
2025-10-24 10:35:41 +0700

    up

--- config/db.php
+++ config/db.php
@@ -1,28 +1,62 @@
-<?php 
+<?php
  class data{
+   // giữ thể hiện duy nhất của class
+    private static $instance =null;
+   //giữ kết nối PDO
+    private $pdo;
+
+    // cấu hình kết nối data
     private $host = "localhost";
-    private $db = "student_mgmt";
-    private $name = "root";
-    private $pass = "";
-    
-    public $conn;
-    
-    public function connect(){
-      $this->conn=null;
-      try{
-         $this->conn = new PDO(
-            "mysql:host=$this->host;dbname=$this->db;charset=utf8mb4",
-            $this->name,
-            $this->pass
-         );
-         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      }catch(PDOException $e){
-         echo "connect failed:" . $e->getMessage();
-      }
-      return $this->conn;
 
-     }
-    
+    private $db ="student_mgmt";
+
+    private $user = "root";
+
+    private $pass ="";
+// private ngăn việc tạo new data mới bên ngoài, chỉ cho phép tạo class này tự khởi động qua getInstance().
+
+   //  private khởi tạo trực tiếp
+    private function __construct(){
+      
+ $connect = "mysql:host=$this->host;dbname=$this->db;charset=utf8mb4";
+  $erro=[
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES =>false,
+
+  ];
+    try{
+      $this->pdo= new PDO($connect,$this->user,$this->pass,$erro);
+    }catch(PDOException $a){
+      die(" connect fail, plase reconnect again".$a->getMessage());
+    }
+   
  }
+ 
 
+ public static function getInstance(){
+  if(self::$dbconnect === null){
+    self::$sbconnect === new Data();
+  }
+  return self::$dbconnect;
+  }
+
+  // getconnect láy đối tượng pdo để truy vấn
+
+  public function getConnection(){
+    return $this->pdo;
+  }
+
+  // __clone ngăn sao chép đối tượng
+
+  private function __clone(){
+  }
+
+  // ___wakeup ngăn unserialize đối tượng, bảo vệ $dbconnect không bị tạo lại qua unserialize()
+
+  private function __wakeup(){
+    throw new Exception("Cannot unserialize singleton.");
+  }
+
+ }
 ?>
