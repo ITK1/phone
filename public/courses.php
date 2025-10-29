55fd7fa686abf05c778a0e2c1eab10c21dbda1ca
ITK <phanlebakhang72@gmail.com>
2025-10-29 21:07:43 +0700

    update

--- public/courses.php
+++ public/courses.php
@@ -1,4 +1,31 @@
 <?php
+require_once '../models/Course.php';
 
-
+$courseModel = new Course();
+$courses = $courseModel->countCourses();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Danh sách khóa học</title>
+</head>
+<body>
+<h2>📘 Danh sách Khóa học</h2>
+<table border="1" cellpadding="5">
+    <tr>
+        <th>ID</th>
+        <th>Tên khóa học</th>
+        <th>Giáo viên</th>
+    </tr>
+    <?php foreach ($courses as $course): ?>
+        <tr>
+            <td><?= $course['id'] ?></td>
+            <td><?= $course['name'] ?></td>
+            <td><?= $course['teacher'] ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
+</body>
+</html>
